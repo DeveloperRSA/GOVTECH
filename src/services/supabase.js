87ddc128d0e9db753c062900bc55d02435d15ce0
Piyoza <132306869+Piyoza@users.noreply.github.com>
2026-09-18 17:32:50 +0200

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://leitzamleuxgnqbwixis.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxlaXR6YW1sZXV4Z25xYndpeGlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk2NjAzNjIsImV4cCI6MjEwNTIzNjM2Mn0.icZQWcNlU0XrmO2UoyRV0VZafEcO5wC9y4F0jzv_WO0';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL; 
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;    
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error(
